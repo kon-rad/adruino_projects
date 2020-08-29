bab6c4de2f66d5fd9b220f98d2193466f7038b6f
konradgnat <konradmgnat@gmail.com>
2020-08-29 14:59:34 -0500

    init

---
+++ 02_spaceship_interface_05242020/02_spaceship_interface_05242020.ino
@@ -0,0 +1,34 @@
+
+int switchState = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(2, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // get state of switch - on or off
+  switchState = digitalRead(2);
+
+  if (switchState == LOW) {
+    // the button is not pressed
+    digitalWrite(3, HIGH); // green LED
+    digitalWrite(4, LOW); // red LED
+    digitalWrite(5, LOW); // red LED
+  } else {
+    // the button is pressed
+    digitalWrite(3, LOW); // green LED
+    digitalWrite(4, LOW); // red LED
+    digitalWrite(5, HIGH); // red LED
+
+    // toggle the LEDs
+    delay(100);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+    delay(100);
+  }
+}
